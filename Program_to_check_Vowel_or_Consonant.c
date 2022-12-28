e6d1fca9d05cc5aefa6374baf41a7aa773049c85
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2022-12-28 23:04:33 +0530

    #include<stdio.h> int main() {     char a,A;     scanf("%c",&a);     if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='o'||A=='U')     {         printf("Vowel");     }         else         {             printf("Consonant");         } }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char a,A;
+    scanf("%c",&a);
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='o'||A=='U')
+    {
+        printf("Vowel");
+    }
+        else
+        {
+            printf("Consonant");
+        }
+}
