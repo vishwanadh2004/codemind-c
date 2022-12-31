54ecb9511b5189754118e33e87988cb4df885c82
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2022-12-31 21:41:34 +0530

    #include<stdio.h> int main() {     long int n1,n2,gcd,lcm,i;     scanf("%ld%ld",&n1,&n2);      for(i=1;i<=n1&&i<=n2;i++)      {          if(n1%i==0&&n2%i==0)          {              gcd=i;          }      }      lcm=(n1*n2)/gcd;      printf("%ld",lcm); }

---
+++ Least_Common_Multiple.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    long int n1,n2,gcd,lcm,i;
+    scanf("%ld%ld",&n1,&n2);
+     for(i=1;i<=n1&&i<=n2;i++)
+     {
+         if(n1%i==0&&n2%i==0)
+         {
+             gcd=i;
+         }
+     }
+     lcm=(n1*n2)/gcd;
+     printf("%ld",lcm);
+}
