1a9f66cd0e4962dd2d94ebfcbf7400d61bcace0c
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-01-05 10:07:10 +0530

    #include<stdio.h> int main() {     float a,b,c;     scanf("%f%f%f",&a,&c,&b);     if(a/b<=c)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    scanf("%f%f%f",&a,&c,&b);
+    if(a/b<=c)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
