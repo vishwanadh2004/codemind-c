3e235cd239047536eab39ce357532971f0e1cc4c
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-01-06 20:18:34 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int n,i,j;
     scanf("%d",&n);
     for(i=n;i>=1;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=n;j>=1;j--)
         {
-            printf("%d",j);
+            printf("%d ",j);
         }
         printf("
 ");
     }
- }
+}
